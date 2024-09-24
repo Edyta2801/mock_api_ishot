0468e65e2f148204fe906dbd6c2251dea6462fdb
Edyta2801 <edyta.szarowska@interia.pl>
2024-09-24 14:19:42 +0200

    fix bugs

--- routes/controllers/sessionController.js
+++ routes/controllers/sessionController.js
@@ -1,4 +1,10 @@
 export function getSessionStatus(req, res) {
     const sessionId = req.query.sessionID;
-    res.json({ sessionID: sessionId, status: 'active' });
+    res.json({
+        data: {
+            type: 'one-two-three',
+            sessionID: sessionId,
+            status: 'Playing',
+        },
+});
 }
